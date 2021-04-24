54be44f78c9a7d4117de6cf16310632abc0f2237
ibarretom <igorbm.23@gmail.com>
2021-04-24 20:56:36 -0300

    gh-pages

---
+++ precache-manifest.a46d2f68b1cbcc4f63a53e622d62f49d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec70f7dd8e404bc5fd86",
+    "url": "/css/app.cac2f698.css"
+  },
+  {
+    "revision": "2b1ca1ecea584c6ba66e",
+    "url": "/css/chunk-vendors.19ac5f0f.css"
+  },
+  {
+    "revision": "182344216c3fb27b5294",
+    "url": "/css/formScreenData.a1bb0a1a.css"
+  },
+  {
+    "revision": "32cb2ec842363c223bbb",
+    "url": "/css/formScreenData~home~quizScreen.0964801c.css"
+  },
+  {
+    "revision": "72c1860f18ad33206423",
+    "url": "/css/formScreenData~quizScreen.7315ce91.css"
+  },
+  {
+    "revision": "00e9a515d701d338c8de",
+    "url": "/css/home.69f380f6.css"
+  },
+  {
+    "revision": "d6eaa673066f59c038ab",
+    "url": "/css/quizScreen.e99349a6.css"
+  },
+  {
+    "revision": "5f03d60ccacad74eecba6f480ec78fce",
+    "url": "/img/avatar-men.5f03d60c.png"
+  },
+  {
+    "revision": "9123286ad34d6bf849bafdc6b1166b4c",
+    "url": "/img/avatar-women.9123286a.png"
+  },
+  {
+    "revision": "2ef959404faadb208ab4a20afe290f5d",
+    "url": "/img/avatar-women2.2ef95940.png"
+  },
+  {
+    "revision": "7e063aab7ffd22cff16e272d3a311047",
+    "url": "/img/head.7e063aab.svg"
+  },
+  {
+    "revision": "fd8549562c182f0a58a3809819e59014",
+    "url": "/img/head.fd854956.png"
+  },
+  {
+    "revision": "898f0edf353fc8eb50725a39f2202f9c",
+    "url": "/img/headicon.898f0edf.svg"
+  },
+  {
+    "revision": "d0b4f4947d6eb0efe4a0d3f99413a7f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec70f7dd8e404bc5fd86",
+    "url": "/js/app.a6412ad8.js"
+  },
+  {
+    "revision": "2b1ca1ecea584c6ba66e",
+    "url": "/js/chunk-vendors.7dcc63de.js"
+  },
+  {
+    "revision": "182344216c3fb27b5294",
+    "url": "/js/formScreenData.c873751e.js"
+  },
+  {
+    "revision": "32cb2ec842363c223bbb",
+    "url": "/js/formScreenData~home~quizScreen.6c09f499.js"
+  },
+  {
+    "revision": "72c1860f18ad33206423",
+    "url": "/js/formScreenData~quizScreen.eb1501ca.js"
+  },
+  {
+    "revision": "00e9a515d701d338c8de",
+    "url": "/js/home.8e95ecb8.js"
+  },
+  {
+    "revision": "d6eaa673066f59c038ab",
+    "url": "/js/quizScreen.b94b34be.js"
+  },
+  {
+    "revision": "4a6b48ddfda5ac42254ddd4e20f41f02",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
